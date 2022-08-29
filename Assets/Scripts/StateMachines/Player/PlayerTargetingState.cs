afa7c07000a31be9e73d9817ad01a5322eda8314
Rithvik Rao <rithvik.rao.8@gmail.com>
2022-08-28 20:19:14 -0700

    Ive added a scene

---
+++ Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTargetingState : PlayerBaseState
+{
+    public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        stateMachine.InputReader.CancelEvent += OnCancel;
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        Debug.Log(stateMachine.Targeter.CurrentTarget.name);
+    }
+
+    public override void Exit()
+    {
+        stateMachine.InputReader.CancelEvent -= OnCancel;
+    }
+
+    private void OnCancel()
+    {
+        stateMachine.Targeter.Cancel();
+
+        stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+    }
+}
